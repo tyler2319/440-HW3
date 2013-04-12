74493329259851c33c554df7bd7dce0221b37996
Tyler Healy <tyler_2319@yahoo.com>
2013-04-11 20:41:20 -0400

    Added a Partitioner class that needs implemeneted

---
+++ src/MapReduceObjects/Partitioner.java
@@ -0,0 +1,5 @@
+package MapReduceObjects;
+
+public class Partitioner {
+
+}
--- src/MapReduceObjects/ReduceWorker.java
+++ src/MapReduceObjects/ReduceWorker.java
@@ -1,8 +1,20 @@
 package MapReduceObjects;
 
+import Config.Configuration;
+
 public class ReduceWorker {
+	
+	private boolean currentlyWorking = false;
 	
 	//TODO Delete - This is just for testing purposes
 	public ReduceWorker() { }
-
+	
+	public void startJob(Configuration config) throws IllegalAccessException {
+		if (!currentlyWorking) {
+			currentlyWorking = true;
+			 
+			currentlyWorking = false;
+		}
+		else throw new IllegalAccessException("Worker already working.");
+	}
 }
