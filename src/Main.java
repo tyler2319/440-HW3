1283e42c20d0473c3f9759289f6843c52b2feabc
Justin Greet <Justin@Justin-PC.(none)>
2013-04-17 22:22:33 -0400

    Change main location in packages.

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import MapReduceObjects.MapReduce440;
+
+
+
+public class Main {
+	
+	public static void main(String[] args) throws Exception {
+		MapReduce440 mr = new MapReduce440(8889, 10, 8881, 10);
+		mr.receiveCommands();
+	}
+}
